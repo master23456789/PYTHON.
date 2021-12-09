930711d34f4e1f95bf692a1c8547a4da29557131
master23456789 <91898768+master23456789@users.noreply.github.com>
2021-12-09 19:29:50 +0530

    Add files via upload

---
+++ pythonfifth.py
@@ -0,0 +1,3 @@
+#Print the characters of a  string from 4th element onwards.
+str = "Lion is the king of the jungle."
+print(str[3:])
---
+++ pythonfourth.py
@@ -0,0 +1,3 @@
+#Declare a string and print it 6 times.
+str = 'Umar has to practise the things.'
+print(str*6)
